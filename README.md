fadf7d71fb9b21241c3eadb826b67f056064bc74
erikmattheis <erik@mattheis.org>
2015-03-22 16:49:31 -0500

    Add CSV reading and formatting

---
+++ README.md
@@ -0,0 +1,7 @@
+The csv-data module expects to find CSVs of price data in
+the csv directory at the top level of the project. The naming
+convention for these files is [symbolName]-[mm/dd/yyy].csv or [option name].csv
+
+The data format should look something like
+Date, Time, Open, High, Low, Close, Volume
+03/09/2015, 08:30, 121.3700, 121.5300, 121.3300, 121.5000, 236330.0000
---
+++ test/io/csv/csv-data-test.js
@@ -0,0 +1,25 @@
+var expect = require("chai").expect,
+	csvData = require("../../../lib/io/csv/csv-data.js");
+
+describe("io/csv/csv-data.js", function() {
+	describe("#read()", function() {
+		it("Should return object with expected properties", function(done) {
+			
+			csvData.read(function(err, bars) {
+				if (err) {
+					console.log(err);
+				}
+				
+				expect(bars[0][0]).to.have.property("symbolName");
+				expect(bars[0][0]).to.have.property("dateTime");
+				expect(bars[0][0]).to.have.property("open");
+				expect(bars[0][0]).to.have.property("high");
+				expect(bars[0][0]).to.have.property("low");
+				expect(bars[0][0]).to.have.property("close");
+				expect(bars[0][0]).to.have.property("volume");
+				done();
+			});
+			
+		});
+	});
+});
