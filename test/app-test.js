1cb5fb15ec36275fd07ef4fc4c143579ba00f782
erikmattheis <erik@mattheis.org>
2015-03-25 23:51:17 -0500

    Added stream from TradeKing

--- test/app-test.js
+++ test/app-test.js
@@ -1,6 +1,6 @@
 var app = require("../lib/app.js");
-/*
 
+/*
 describe("App", function() {
 	describe("#init()", function() {
 		it("Should emmit object with expected properties", function(done) {
@@ -12,17 +12,5 @@ describe("App", function() {
 			
 		});
 	});
-});
-
-describe('Testing', function(){
-  it('Should send an object with a testing key', function(done){
-    res.on('response', function(resp) {
-      expect(resp.response).to.have.property('testing');
-      expect(resp.response.testing).to.equal(true);
-      done();
-    });
-    
-    testing.test({}, res);
-  });
 });
 */
---
+++ test/io/exchange/TradeKing/stream-test.js
@@ -0,0 +1,42 @@
+var expect = require("chai").expect,
+	EventEmitter = require('events').EventEmitter,
+	stream = require("../../../../lib/io/exchange/TradeKing/stream.js");
+
+describe("lib/io/exchange/TradeKing/stream.js", function() {
+	describe("#init()", function() {
+		it("Should connect to TradeKing stream without error", function(done) {
+			
+			stream.init(["IWM"], function(err, emitter) {
+				if (err) {
+					console.log(err);
+				}
+				stream.getEmitter().on("data", function(data) {
+					console.log("event", data);
+					
+					expect(data).to.have.property("status", "connected");
+				});
+				
+				done();
+			});
+		});
+		/*
+		it("Should return object with expected properties", function(done) {
+			
+			stream.stream(function(err, bars) {
+				if (err) {
+					console.log(err);
+				}
+				
+				expect(bars[0][0]).to.have.property("symbolName");
+				expect(bars[0][0]).to.have.property("dateTime");
+				expect(bars[0][0]).to.have.property("open");
+				expect(bars[0][0]).to.have.property("high");
+				expect(bars[0][0]).to.have.property("low");
+				expect(bars[0][0]).to.have.property("close");
+				expect(bars[0][0]).to.have.property("volume");
+				done();
+			});
+		});
+*/
+	});
+});
