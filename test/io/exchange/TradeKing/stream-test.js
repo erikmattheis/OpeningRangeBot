e4024e7261c49d93239dffac69ee465aa221c80f
erikmattheis <erik@mattheis.org>
2015-03-28 19:11:14 -0500

    Bug fixes and refactoring

--- test/io/exchange/TradeKing/stream-test.js
+++ test/io/exchange/TradeKing/stream-test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var expect = require("chai").expect,
 	EventEmitter = require('events').EventEmitter,
 	stream = require("../../../../lib/io/exchange/TradeKing/stream.js");
@@ -10,33 +12,12 @@ describe("lib/io/exchange/TradeKing/stream.js", function() {
 				if (err) {
 					console.log(err);
 				}
-				stream.getEmitter().on("data", function(data) {
-					//console.log("event", data);
-					
+				stream.getEmitter().on("data", function(data) {					
 					expect(data).to.have.property("status", "connected");
 				});
 				
 				done();
 			});
 		});
-		/*
-		it("Should return object with expected properties", function(done) {
-			
-			stream.stream(function(err, bars) {
-				if (err) {
-					console.log(err);
-				}
-				
-				expect(bars[0][0]).to.have.property("symbolName");
-				expect(bars[0][0]).to.have.property("dateTime");
-				expect(bars[0][0]).to.have.property("open");
-				expect(bars[0][0]).to.have.property("high");
-				expect(bars[0][0]).to.have.property("low");
-				expect(bars[0][0]).to.have.property("close");
-				expect(bars[0][0]).to.have.property("volume");
-				done();
-			});
-		});
-*/
 	});
 });
